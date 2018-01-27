b29cbbbcfc03140f9781835d0d1038b0cd1a99da
aaronlelevier <pyaaron@gmail.com>
2018-01-27 07:15:41 -0800

    [Slot Reel] 2 Reels with win / loss console log working

--- app/components/slot-machine.js
+++ app/components/slot-machine.js
@@ -1,11 +1,42 @@
 import Component from '@ember/component';
+import { get, set } from '@ember/object';
+
+var REEL = [];
 
 export default Component.extend({
+  /*
+   * Sets up Component VARs:
+   * - machine1, machine2
+   */
   didInsertElement: function() {
-    let slotMachine = this.$('.machine1').slotMachine({
+    let machine1 = this.$('.machine1').slotMachine({
       active: 0,
-      delay: 500
+      delay: 100
     });
-    this.set('slotmachine', slotMachine);
+    set(this, 'machine1', machine1);
+    let machine2 = this.$('.machine2').slotMachine({
+      active: 1,
+      delay: 200
+    });
+    set(this, 'machine2', machine2);
+  },
+  onComplete(active){
+    REEL.push(active);
+    if (REEL.length == 2) {
+      if (REEL[0] == REEL[1]) {
+        console.log(`win: ${REEL[0]} ${REEL[1]}`);
+      } else {
+        console.log(`loss: ${REEL[0]} ${REEL[1]}`);
+      }
+    }
+  },
+  actions: {
+    shuffle() {
+      // clear rell for start
+      REEL = [];
+      // shuffle slot machine
+      get(this, 'machine1').shuffle(5, this.onComplete);
+      get(this, 'machine2').shuffle(5, this.onComplete);
+    }
   }
 });
