0219b863a66766e73391f5e51152da8e7d115da0
aaronlelevier <pyaaron@gmail.com>
2018-01-28 06:42:03 -0800

    [slot-machine] consolidated some method duplication for setting Reel elements in main 'slot-machine' component

--- app/components/slot-machine.js
+++ app/components/slot-machine.js
@@ -1,42 +1,33 @@
 import Component from '@ember/component';
-import {get,
+import {
+  get,
   set
 } from '@ember/object';
 
 var RESULTS = [];
 
 export default Component.extend({
-  /*
-   * Sets up Component VARs:
-   * - reel1, reel2
-   */
   didInsertElement: function() {
-    let reel1 = this.$('.reel1').slotMachine({
-      active: 0,
-      delay: 100
+    this.setReel('reel1', 0, 100);
+    this.setReel('reel2', 0, 200);
+    this.setReel('reel3', 0, 300);
+  },
+  setReel(name, active, delay) {
+    let reel = this.$(`.${name}`).slotMachine({
+      active,
+      delay
     });
-    set(this, 'reel1', reel1);
-
-    let reel2 = this.$('.reel2').slotMachine({
-      active: 1,
-      delay: 200
-    });
-    set(this, 'reel2', reel2);
-
-    let reel3 = this.$('.reel3').slotMachine({
-      active: 1,
-      delay: 300
-    });
-    set(this, 'reel3', reel3);
+    set(this, name, reel);
   },
   onComplete(active) {
     RESULTS.push(active);
-    if (RESULTS.length == 2) {
+    if (RESULTS.length == 3) {
       if (RESULTS[0] == RESULTS[1]) {
-        console.log(`win: ${RESULTS[0]} ${RESULTS[1]}`);
+        console.log(`win`);
       } else {
-        console.log(`loss: ${RESULTS[0]} ${RESULTS[1]}`);
+        console.log(`loss`);
       }
+      console.log(`${RESULTS[0]} ${RESULTS[1]} ${RESULTS[2]}`);
     }
   },
   actions: {
