2c055829c0de4f957c62301b54eb54a474861687
aaronlelevier <pyaaron@gmail.com>
2018-01-28 08:05:16 -0800

    [Ember build config] adjust so can properly do 'build ember'

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'slotmachine',
     environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: '',
+    locationType: 'none',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
